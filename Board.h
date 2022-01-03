aaf4f32b5c5300ee58194bcd7580c9d2a031bd26
alexandraantohi <73305226+alexandraantohi@users.noreply.github.com>
2022-01-03 19:28:33 +0200

    adauga structura pentru joc

--- Board.h
+++ Board.h
@@ -6,8 +6,9 @@
 #include <stdio.h>
 #include<iostream>
 #include "gameOver.h"
+#include "joc.h"
 using namespace std;
-
+/*
 int margin = 70, boxsize = 70, dificultate = 1;
 
 struct animal {
@@ -15,12 +16,23 @@ struct animal {
 };
 struct animal dogs[4];
 struct animal fox;
+*/
+struct optiuniJoc joc;
 
+void initJoc() {
+    joc.margin = 70;
+    joc.boxsize = 70;
+    joc.dificultate = 1;
+    joc.urmeazaMiscare = false;
+    joc.aFostFoxUltimaMiscare = false;
+    joc.counter_dog = -1;
+
+}
 
 void drawPiece(int a, int b, int color){
     //deseneaza piesa pe tabla la pozitia a, b de culoarea color
-    int x=margin + a*boxsize+boxsize/2;
-    int y=margin + b*boxsize+boxsize/2;
+    int x=joc.margin + a*joc.boxsize+ joc.boxsize/2;
+    int y=joc.margin + b* joc.boxsize+ joc.boxsize/2;
     setcolor(color);
     setfillstyle(SOLID_FILL,color);
     circle(x,y,10);
@@ -30,20 +42,20 @@ void drawPiece(int a, int b, int color){
 void drawAnimals(){
     //deseneaza pe tabla toate piesele
     for(int i=0; i< 4; i++){
-        drawPiece(dogs[i].x,dogs[i].y,dogs[i].color);
+        drawPiece(joc.dogs[i].x,joc.dogs[i].y,joc.dogs[i].color);
     }
-    drawPiece(fox.x, fox.y,fox.color);
+    drawPiece(joc.fox.x, joc.fox.y,joc.fox.color);
 }
 
 void initAnimals(){
     //initializeaza pozitiile de start pentru joc
-    fox.x=3;
-    fox.y=0;
-    fox.color = RED;
+    joc.fox.x=3;
+    joc.fox.y=0;
+    joc.fox.color = RED;
     for(int i=0; i< 4; i++){
-        dogs[i].x=2*i;
-        dogs[i].y=7;
-        dogs[i].color = BLUE;
+        joc.dogs[i].x=2*i;
+        joc.dogs[i].y=7;
+        joc.dogs[i].color = BLUE;
     }
 }
 
@@ -59,9 +71,9 @@ bool celulaOcupata(int x,int y){
     if(x<0 || x>7 || y<0 || y>7)
         return true; // in afara tablei
     for(int i=0;i<4;i++)
-        if(dogs[i].x == x && dogs[i].y == y)
+        if(joc.dogs[i].x == x && joc.dogs[i].y == y)
             return true;
-    if(fox.x == x && fox.y == y )
+    if(joc.fox.x == x && joc.fox.y == y )
         return true;
     return false;
 }
@@ -100,19 +112,21 @@ bool movement_fox(animal &fox, int x, int y){
     movement(fox,x,y);
 }
 
+/*
 bool urmeazaMiscare = false;
 bool aFostFoxUltimaMiscare = false;
 int xSelectat,ySelectat;
+*/
 
 int verificaScenariuCastig()
 {
-    if(fox.y == 7) // daca a ajuns pe ultimul rand, a castigat fox
+    if(joc.fox.y == 7) // daca a ajuns pe ultimul rand, a castigat fox
         return 2;
     int newX,newY,pozitiiFox[] = {-1,-1,  -1,1,   1,-1,   1,1}; // mutari posibile
     for(int i=0; i<8; i+=2)
     {
-        newX = fox.x + pozitiiFox[i];
-        newY = fox.y + pozitiiFox[i+1];
+        newX = joc.fox.x + pozitiiFox[i];
+        newY = joc.fox.y + pozitiiFox[i+1];
         if(newX >= 0 && newX <8 && newY >= 0 && newY <8)
             if(!celulaOcupata(newX, newY))
                 return 0;//daca fox are unde sa se duca
@@ -128,23 +142,23 @@ void closeBoard()
 
 bool transforma_coordonate_in_pozitie(int &x, int &y){
     //calculeaza in care celula de pe tabla a fost dat click
-    if(x<margin || y<margin || x>margin + 8*boxsize|| y>margin+8*boxsize) return false;//verifica daca click-ul a fost pe tabla si nu pe langa
-    x=x-margin;
-    x=x/boxsize;
-    y=y-margin;
-    y=y/boxsize;
+    if(x<joc.margin || y<joc.margin || x>joc.margin + 8* joc.boxsize|| y>joc.margin+8* joc.boxsize) return false;//verifica daca click-ul a fost pe tabla si nu pe langa
+    x=x-joc.margin;
+    x=x/joc.boxsize;
+    y=y-joc.margin;
+    y=y/joc.boxsize;
     return true;
 }
 
 void muta_piesa(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y){
     int castig;
-    if(!aFostFoxUltimaMiscare)
+    if(!joc.aFostFoxUltimaMiscare)
         {
-            if(sursa_x == fox.x && sursa_y == fox.y)
+            if(sursa_x == joc.fox.x && sursa_y == joc.fox.y)
             {
-                if(movement_fox(fox,destinatie_x,destinatie_y))
+                if(movement_fox(joc.fox,destinatie_x,destinatie_y))
                 {
-                    aFostFoxUltimaMiscare = true;
+                    joc.aFostFoxUltimaMiscare = true;
                 }
             }
         }
@@ -152,11 +166,11 @@ void muta_piesa(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y){
     {
         for(int i=0; i<4; i++)
         {
-            if(sursa_x == dogs[i].x && sursa_y == dogs[i].y) // daca a fost selectat un dog
+            if(sursa_x == joc.dogs[i].x && sursa_y == joc.dogs[i].y) // daca a fost selectat un dog
             {
-                if(movement_dog(dogs[i],destinatie_x,destinatie_y)) // daca a mutat dog cu succes
+                if(movement_dog(joc.dogs[i],destinatie_x,destinatie_y)) // daca a mutat dog cu succes
                 {
-                    aFostFoxUltimaMiscare = false;
+                    joc.aFostFoxUltimaMiscare = false;
                 }
                 // else
                 // {
@@ -177,16 +191,17 @@ void muta_piesa(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y){
     }
 }
 
-int counter_dog = -1;
+//int counter_dog = -1;
 void muta_piesa_pvc(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y){
     int castig;
 
-    if (sursa_x == fox.x && sursa_y == fox.y)
+    if (sursa_x == joc.fox.x && sursa_y == joc.fox.y)
     {
-        if (movement_fox(fox, destinatie_x, destinatie_y))
+        if (movement_fox(joc.fox, destinatie_x, destinatie_y))
         {
-            aFostFoxUltimaMiscare = true;
+            joc.aFostFoxUltimaMiscare = true;
         }
+        else return;
     }
     else return;
 
@@ -195,29 +210,29 @@ void muta_piesa_pvc(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y
     int new_x, new_x_dificultate_2, new_y;
     while (!ok)
     {
-        counter_dog++;
-        counter_dog = counter_dog % 4; //trece la urmatorul caine si de la capat
-        new_y = dogs[counter_dog].y -1;
-        if (dogs[counter_dog].y % 2 != 0)
+        joc.counter_dog++;
+        joc.counter_dog = joc.counter_dog % 4; //trece la urmatorul caine si de la capat
+        new_y = joc.dogs[joc.counter_dog].y -1;
+        if (joc.dogs[joc.counter_dog].y % 2 != 0)
         {
             //facem mutare la dreapta
-            new_x = dogs[counter_dog].x + 1;
-            new_x_dificultate_2 = dogs[counter_dog].x - 1;
+            new_x = joc.dogs[joc.counter_dog].x + 1;
+            new_x_dificultate_2 = joc.dogs[joc.counter_dog].x - 1;
         }
         else
         {
             //face mutare la stanga
-            new_x = dogs[counter_dog].x - 1;
-            new_x_dificultate_2 = dogs[counter_dog].x + 1;
+            new_x = joc.dogs[joc.counter_dog].x - 1;
+            new_x_dificultate_2 = joc.dogs[joc.counter_dog].x + 1;
         }
-        if(dificultate == 0)
+        if(joc.dificultate == 0)
         {
             if (celulaOcupata(new_x, new_y) == false)
             {
-                movement_dog(dogs[counter_dog], new_x, new_y);
+                movement_dog(joc.dogs[joc.counter_dog], new_x, new_y);
                 ok = true;
             }
-        } 
+        }
         else
         //pe dificultate 1 daca stanga, dreapta sunt libere si diferite de vulpe ca sa putem muta un caine
         {
@@ -225,7 +240,7 @@ void muta_piesa_pvc(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y
             pas_1 = !celulaOcupata(new_x, new_y);
             if (new_x_dificultate_2 >= 0 && new_x_dificultate_2 < 8)
             {
-                if(fox.x == new_x_dificultate_2 && fox.y == new_y)
+                if(joc.fox.x == new_x_dificultate_2 && joc.fox.y == new_y)
                 {
                     pas_2 = false;
                 }
@@ -238,10 +253,10 @@ void muta_piesa_pvc(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y
             {
                 pas_2 = true;
             }
-            
+
             if (pas_1 && pas_2)
             {
-                movement_dog(dogs[counter_dog], new_x, new_y);
+                movement_dog(joc.dogs[joc.counter_dog], new_x, new_y);
                 ok = true;
             }
         }
@@ -260,18 +275,18 @@ void muta_piesa_pvc(int sursa_x, int sursa_y, int destinatie_x, int destinatie_y
 
 void click_select(int x, int y){
     if(!transforma_coordonate_in_pozitie(x,y)) return;
-    xSelectat = x;
-    ySelectat = y;
+    joc.xSelectat = x;
+    joc.ySelectat = y;
 }
 
 void click_drop(int x, int y){
     if(!transforma_coordonate_in_pozitie(x,y)) return;
-    muta_piesa(xSelectat, ySelectat, x, y);
+    muta_piesa(joc.xSelectat, joc.ySelectat, x, y);
 }
 
 void click_drop_pvc(int x, int y){
     if(!transforma_coordonate_in_pozitie(x,y)) return;
-    muta_piesa_pvc(xSelectat, ySelectat, x, y);
+    muta_piesa_pvc(joc.xSelectat, joc.ySelectat, x, y);
 }
 
 void generateBoard()
@@ -279,29 +294,29 @@ void generateBoard()
     //deseneaza tabla de sah si piesele pe tabla
     int gr = DETECT, gm;
     int r, c, x, y;
-    x=margin;
-    y=margin;
+    x=joc.margin;
+    y=joc.margin;
     bool black = false;
     initwindow(720, 720);
     for (r = 0; r < 8; r++) {
         for (c = 1; c <= 8; c++) {
             if (black) {
                 setfillstyle(SOLID_FILL, BLACK);
-                rectangle(x, y, x + boxsize, y + boxsize);
+                rectangle(x, y, x + joc.boxsize, y + joc.boxsize);
                 floodfill(x + 1, y + 1, WHITE);
             }
             else {
                 setfillstyle(SOLID_FILL, WHITE);
-                rectangle(x, y, x + boxsize, y + boxsize);
+                rectangle(x, y, x + joc.boxsize, y + joc.boxsize);
                 floodfill(x + 1, y + 1, WHITE);
             }
-            x = x + boxsize;
+            x = x + joc.boxsize;
             black=!black;
         }
         black=!black;
 
-        x = margin;
-        y = boxsize + y;
+        x = joc.margin;
+        y = joc.boxsize + y;
     }
     //drawPiece(0,0);
     //drawPiece(0,1);
@@ -320,6 +335,7 @@ void generateBoard()
 
 void joc_pvp(){
     timp.start=time(NULL);
+    initJoc();
     generateBoard();
     initAnimals();
     drawAnimals();
@@ -328,11 +344,12 @@ void joc_pvp(){
     registermousehandler(WM_LBUTTONUP, click_drop);
 
     closeBoard();
-   
+
 }
 
 void joc_pvc() {
     timp.start=time(NULL);
+    initJoc();
     generateBoard();
     initAnimals();
     drawAnimals();
